76304ff4123ba6c0ebe708595d7e4102951fb79c
OsvaldoDJesus <osvalodjesu@gmail.com>
2026-01-28 05:14:03 -0500

    feat: integrar provider de Redux en la aplicación
    - Envolver el componente App con el Provider de Redux
    - Envolver con ErrorBoundary para manejo de errores
    - Importar estilos globales
    - Asegurar que el store de Redux esté disponible para todas las páginas

---
+++ src/pages/_app.tsx
@@ -0,0 +1,11 @@
+import type { AppProps } from 'next/app';
+import { Provider } from '@/store/Provider';
+import '@/styles/globals.css';
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <Provider>
+      <Component {...pageProps} />
+    </Provider>
+  );
+}
